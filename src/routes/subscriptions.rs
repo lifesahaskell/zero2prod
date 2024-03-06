e53c9b29fe6362c6c0d71841192f83dad0c98e5f
Corey Todalen <ctodalen@gmail.com>
2024-03-05 20:45:02 -0600

    formatting

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,27 +14,18 @@ pub struct FormData {
     skip(form, pool),
     fields(
         subscriber_email = %form.email,
-        subscriber_name = %form.name    
+        subscriber_name = %form.name
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    
-    match insert_subscriber(&pool, &form).await
-    {
+    match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish()
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-
-#[tracing::instrument(
-    name = "Saving new subscriber details in the db",
-    skip(form, pool)
-)]
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    form: &FormData,
-) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving new subscriber details in the db", skip(form, pool))]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -4,12 +4,12 @@ use tracing_log::LogTracer;
 use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
 
 pub fn get_subscriber<Sink>(
-    name: String, 
-    env_filter: String, 
-    sink: Sink
+    name: String,
+    env_filter: String,
+    sink: Sink,
 ) -> impl Subscriber + Send + Sync
-    where
-        Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
 {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
