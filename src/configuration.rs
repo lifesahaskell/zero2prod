ab1c662430e41c94ed16eaf5362f1f2aafb4c4a9
Corey Todalen <ctodalen@gmail.com>
2025-02-13 21:27:51 -0600

    added domain.rs module and do some light email validation

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,4 @@
-use secrecy::{ExposeSecret, Secret};
+use secrecy::{ExposeSecret, SecretString};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::PgConnectOptions;
 use sqlx::postgres::PgSslMode;
@@ -14,7 +14,7 @@ pub struct Settings {
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
-    pub password: Secret<String>,
+    pub password: SecretString,
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
